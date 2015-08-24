facb5e4c7efcb82aaab33b8dbf67bba36d2a52e2
JuneBug2013 <wj1983ise@gmail.com>
2015-08-23 19:05:18 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,35 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Below are two functions that are used to create a special object 
+##  that stores a matrix and cache's its inverse matrix.
 
-## Write a short comment describing this function
+## This function "makeMatrix" creates a special "matrix", 
+## which is really a list containing several functions. 
 
-makeCacheMatrix <- function(x = matrix()) {
- t <- NULL
-  set <- function(y) {
+makeMatrix <- function(x = matrix()) {
+  t <- NULL
+  set <- function(y) {  # set the value of the matrix
     x <<- y
     t <<- NULL
   }
-  get <- function() x
-  settranspose <- function(transpose) t <<- transpose
-  gettranspose <- function() t
+  get <- function() x  # get the value of the matrix
+  setinverse <- function(inverse) t <<- inverse  # set the value of the inverse matrix
+  getinverse <- function() t # get the value of the inverse matrix 
   list(set = set, get = get,
-       settranspose = settranspose,
-       gettranspose = gettranspose) 
-
+       setinverse = setinverse,
+       getinverse = getinverse) 
+  
 }
 
 
-## Write a short comment describing this function
+## This function "cacheSolve" calculates the inverse matrix of the special 
+## "matrix" created with the above "makeMatrix" function. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-  t <- x$transpose()
-  if(!is.null(t)) {
-    message("getting cached data")
-    return(t)
+  ## Return a matrix that is the inverse of 'x'
+  t <- x$getinverse() # check if the inverse matrix has already been calculated
+  if(!is.null(t)) {   # if it has been caculated, it gets the inverse matrix from the cache
+    message("getting cached data") 
+    return(t)    # skip the caculation.
   }
-  data <- x$get()   # get the data 
+  data <- x$get()   # get the matrix
   t <- solve(data, ...) # calculate the inverse matrix
-  x$settranspose(t) # put it into cashe
-  t
-}
+  x$setinverse(t) # put the value of the inverse matrix into cache
+  
